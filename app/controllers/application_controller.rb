d8b1c006298e666601ab700b07ef4636ea68b0bd
christianstanfield <christian.stanfield@gmail.com>
2015-04-17 15:35:32 -0500

    added token auth to user and app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user_from_token!
+
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
     new_user_password_path(resource)
+  end
+
+  private
+
+  def authenticate_user_from_token!
+    user_email = params[:user_email].presence
+    user       = user_email && User.find_by_email(user_email)
+
+    if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+      sign_in user, store: false
+    end
   end
 end
